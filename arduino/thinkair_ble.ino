c66fd4ef2f83a3f7311a5d5f86cdb85958c163bc
pezzaliapp <pezzalialessandro@gmail.com>
2025-04-24 14:58:18 +0200

    Add files via upload

--- arduino/thinkair_ble.ino
+++ arduino/thinkair_ble.ino
@@ -1,1 +1,34 @@
+#include "Arduino_BHY2.h"
 
+Sensor temperature(SENSOR_ID_TEMP);
+Sensor humidity(SENSOR_ID_HUM);
+Sensor pressure(SENSOR_ID_BARO);
+Sensor gasRes(SENSOR_ID_GAS);
+
+float offset = -4.5;
+
+void setup() {
+  Serial.begin(115200);
+  BHY2.begin();
+  temperature.begin();
+  humidity.begin();
+  pressure.begin();
+  gasRes.begin();
+}
+
+void loop() {
+  BHY2.update();
+
+  float temp = temperature.value() + offset;
+  float hum = humidity.value();
+  float pres = pressure.value();
+  float gas = gasRes.value();
+
+  Serial.println("---------");
+  Serial.print("Temp: "); Serial.println(temp);
+  Serial.print("Hum: "); Serial.println(hum);
+  Serial.print("Pres: "); Serial.println(pres);
+  Serial.print("Gas: "); Serial.println(gas);
+
+  delay(2000);
+}
